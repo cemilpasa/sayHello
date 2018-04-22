cd1fa610028353773dbafc9bc4a28d16636c5329
Cemil <cco@riesenrat.de>
2018-04-22 21:05:06 +0200

    1
    1

--- src/Providers/TopItemsServiceProvider.php
+++ src/Providers/TopItemsServiceProvider.php
@@ -7,9 +7,6 @@ use Plenty\Plugin\ServiceProvider;
 
 class TopItemsServiceProvider extends ServiceProvider
 {
-
-
-
     public function register()
     {
         $this->getApplication()->register(TopItemsRouteServiceProvider::class);
