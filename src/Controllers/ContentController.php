d6f663a36fa332185293ce73812329c4533c4be4
Cemil <cco@riesenrat.de>
2018-04-22 18:33:21 +0200

    b

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace HelloWorld\Controllers;
+namespace apiMarketPlaces\Controllers;
 
 
 use Plenty\Plugin\Controller;
@@ -10,6 +10,6 @@ class ContentController extends Controller
 {
     public function sayHello(Twig $twig):string
     {
-        return $twig->render('HelloWorld::content.hello');
+        return $twig->render('apiMarketPlaces::content.hello');
     }
 }
