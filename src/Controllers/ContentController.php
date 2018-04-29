4702abfee3089a6743b4ebc1296a92eeec44b5b7
cemilpasa <38496626+cemilpasa@users.noreply.github.com>
2018-04-29 18:43:52 +0200

    .

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -32,7 +32,7 @@ class ContentController extends Controller
 
         $itemFilter = [
             'itemBase.isStoreSpecial' => [
-                'shopAction' => [1]
+                'shopAction' => [2]
             ]
         ];
 
